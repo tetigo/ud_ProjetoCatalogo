5ce937c56b8a2def90e5b77bc605452fdd6f4219
Tiago <tetigo@gmail.com>
2019-03-20 11:59:51 -0300

    Update carga_banco.py

--- carga_banco.py
+++ carga_banco.py
@@ -1,20 +1,17 @@
 from app import db
 from models import User, Category, Item
 
-#c1 = Category()
-#c2 = Category()
-
 exists = Category.query.filter_by(name='Hockey').first()
 if not exists:
-	c1 = Category()
-	c1.name = 'Hockey'
-	db.session.add(c1)
+    c1 = Category()
+    c1.name = 'Hockey'
+    db.session.add(c1)
 
 exists = Category.query.filter_by(name='Baseball').first()
 if not exists:
-	c2 = Category()
-	c2.name = 'Baseball'
-	db.session.add(c2)
+    c2 = Category()
+    c2.name = 'Baseball'
+    db.session.add(c2)
 
 db.session.commit()
 
